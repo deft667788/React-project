89495d07810ca0a818f98242a38437a5dc013d50
Allen <hanwen.shi667788@gmail.com>
2024-05-03 11:34:21 +0800

    material ui and react router dom dependency added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Login = () => {
+  return <div>Login!</div>;
+};
+
+const Home = () => {
+  return <div>Home!</div>;
+};
 
 function App () {
   return (
-    <>Let&apos;s go!</>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
